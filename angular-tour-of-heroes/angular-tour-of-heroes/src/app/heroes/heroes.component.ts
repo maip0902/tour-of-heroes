f7575e99194706fb6ce9a1f36352e956a3b8985b
神田舞 <maiptomato@gmail.com>
2022-04-18 10:58:56 +0900

    serviceの追加

--- angular-tour-of-heroes/angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ angular-tour-of-heroes/angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
 import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
@@ -8,16 +9,21 @@ import { HEROES } from '../mock-heroes';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-  heroes: Hero[] = HEROES
+  heroes: Hero[] = []
   selectedHero?: Hero;
 
-  constructor() { }
+  constructor(private heroService: HeroService) { }
 
   ngOnInit(): void {
+    this.getHeroes()
   }
 
   onSelect(hero: Hero) {
     this.selectedHero = hero
+  }
+
+  getHeroes(): void {
+    this.heroes = this.heroService.getHeroes();
   }
 
 }
